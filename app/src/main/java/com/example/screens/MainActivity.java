ba0729d0ddc83d694662f7019a649d72cf0770b2
b23ebbst <b23ebbst@his.student.se>
2024-04-15 15:08:34 +0200

    Aktviverat knappen så den leder till den sekundära aktiviteten.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.screens;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.button);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>","You've been taken away!");
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("name", "Daniel"); // Optional
+                intent.putExtra("number", 1); // Optional
+                startActivity(intent);
+            }
+        });
     }
 }
